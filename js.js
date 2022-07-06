4037ca5477a8402efd0f672b79bf75a6684ae41d
flylessbird <chi.ptl185329@sis.hust.edu.vn>
2022-07-06 10:36:41 +0700

    ver1

--- js.js
+++ js.js
@@ -3,22 +3,23 @@ var toogleBtn = document.querySelector(".toogle-icon")
 var optionHidden = document.querySelectorAll(".show")
 var buyBtn = document.querySelectorAll(".buy-btn")
 var ticket = document.querySelector(".ticket")
-var closeBtn = document.querySelector(".close")
+// var closeBtn = document.querySelector(".close")
 for (let i = 0; i < buyBtn.length; i++) {
     buyBtn[i].addEventListener("click", function () {
         ticket.style.display = "block"
     })
 }
-closeBtn.addEventListener("click",function(){
+function closeFunc() {
     ticket.style.display = "none"
 
-})
+}
 function myFunction() {
     if (option.className == "option") {
         option.className += " option-toggle"
 
         for (let i = 0; i < optionHidden.length; i++) {
             optionHidden[i].style.display = "block"
+            optionHidden[i].classList.add("show-icon")
         }
     }
     else {
